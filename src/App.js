1fb8c3ea4077f2d81ae78b7ceecef8ff4cf86c9a
b4c1c <b4c1c@b4c1c-device.local>
2023-01-26 12:22:31 +0100

    small changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Trying to upload react project to github pages!
         </p>
         <a
           className="App-link"
